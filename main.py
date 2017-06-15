cddd101de620a0162dd8bfe9908258af68b5a141
c20xh2 <c20xh2@gmail.com>
2017-06-15 14:47:47 -0400

    Update main.py

--- main.py
+++ main.py
@@ -4,11 +4,13 @@ import subprocess
 import os
 import sys
 import argparse
+from crontab import CronTab
+
 
 def framecapture(username, password, protocol, camerapath, interval, nbframe, created, savepath, i):
-	camera = str(protocol) + "://" + str(username) + ":" + str(password) + "@" + str(camerapath)
+	camera = str(protocol) + '://' + str(username) + ':' + str(password) + '@' + str(camerapath)
 	vcap = cv2.VideoCapture(camera)
-	filename  = savepath + "/" + str(i) + ".png"
+	filename  = savepath + '/' + str(i) + '.png'
 	grabbed, frame = vcap.read()
 	cv2.imwrite(filename, frame)
 	statinfo = os.stat(filename)
@@ -17,10 +19,10 @@ def framecapture(username, password, protocol, camerapath, interval, nbframe, cr
 		i = i - 1
 	
 def videoexport(fps, finalname, created, outputpath, savepath):
-	print("\n[+] Creating video file please wait... ")
+	print('\n[+] Creating video file please wait... ')
 	FNULL = open(os.devnull, 'w')
-	subprocess.call(["avconv", "-r", str(fps), "-i", str(savepath) + '/%d.png', '-c:v', 'libx264', '-pix_fmt', 'yuv420p', str(outputpath) + str(finalname)], stdout=FNULL, stderr=subprocess.STDOUT)
-	print("\n[!] Congrats ! Filename: " + str(finalname))
+	subprocess.call(['avconv', '-r', str(fps), '-i', str(savepath) + '/%d.png', '-c:v', 'libx264', '-pix_fmt', 'yuv420p', str(outputpath) + str(finalname)], stdout=FNULL, stderr=subprocess.STDOUT)
+	print('\n[!] Congrats ! Filename: ' + str(finalname))
 
 # Argument parsing
 
@@ -48,21 +50,22 @@ correction = args.correction
 
 # Camera info
 
-camera = str(protocol) + "://" + str(username) + ":" + str(password) + "@" + str(camerapath)
+camera = str(protocol) + '://' + str(username) + ':' + str(password) + '@' + str(camerapath)
 cameraip = camerapath.split('/')[0]
-resetcommand = ("http://" + str(username) + ":" + str(password) + "@" + str(cameraip) + ":80/hy-cgi/device.cgi?cmd=sysreboot")
+resetcommand = ('http://' + str(username) + ':' + str(password) + '@' + str(cameraip) + ':80/hy-cgi/device.cgi?cmd=sysreboot')
 
 # Get video info
 
-created = time.strftime("%D")
+created = time.strftime('%D')
 created = created.replace('/', '')
 
-finalname = (str(created) + "-" + str(fps) + "-" + str(length) + "-" + str(eventlast) + ".mp4" )
+finalname = (str(created) + '-' + str(fps) + '-' + str(length) + '-' + str(eventlast) + '.mp4' )
 
-
+launched = time.strftime('%y%m%d%H%M')
 
 # make sure savepath exist
-savepath = "autopng/" + str(created)
+
+savepath = 'autopng/' + str(launched)
 outputpath = 'output/'
 logpath = 'logs/'
 try: 
@@ -93,26 +96,95 @@ if interval < 1:
 	interval = 0
 # Validate infos before starting capture
 i = 0
-launched = time.strftime("%c")
 
-with open(str(logpath) + str(created), 'a') as logfile:
-	log = ("\n" + str(finalname) + "\nFps:" + str(fps) + "\nEventlast:" + str(eventlast) + "\nFinalVideo:" + str(length) + "\nNbframe:" + str(nbframe) + "\nInterval:" + str(askedinterval))
-	logfile.write(log)
-	logfile.close()
 
-while (i < 30):
+print ("\n" *200)
 
-	try:
+print ("###### Validations ######")
+print('[+] Camera : ' + str(camera) )
+print(' [|] Network lag correction: ' + str(correction) )
 
-		# Get info for how long left
-		frameleft = nbframe - i
-		timeleft = frameleft * askedinterval / 60
-		timeleft = round(timeleft, 0)
-		# Grab the frame
-		framecapture(username, password, protocol, camerapath, interval, nbframe, created, savepath, i)
-		time.sleep(interval)
-		i = i + 1
-	except Exception as e:
-		print (e)
-# Capture is over, putting all the images together to create video file
-videoexport(fps, finalname, created, outputpath)
+print ("\n[+] Output file name: " + str(finalname))
+print (" [|] Event length: " + str(eventlast) + " h.")
+print (" [|] Final Video length: " + str(length) + " sec.")
+
+print ("\n[+] Video specs: ")
+print (" [|] Fps: " + str(fps) +".")
+print (" [|] Number of frames: " + str(nbframe) + ".") 
+print (" [|] Interval between shots: " + str(askedinterval) + " sec.")
+print ("")
+
+launch = input('[*] Ready ? (y/n) : ')
+
+if launch == ('n'):
+	print ('\nAbording Capture...')
+
+elif launch == ('y'):
+	cron = input('\n[*] Schedule task ? (y/n) : ')
+	if cron == ('y'):
+		print ('\n')
+		dir_path = os.path.dirname(os.path.realpath(__file__))
+		cronuser = input('[*] Please enter username: ')
+		cronwhen = input('[*] Launch time Hours:Minute (use 24h format) :')
+
+		hour = cronwhen.split(':')[0]
+		minutes = cronwhen.split(':')[1]
+
+		my_cron = CronTab(user=cronuser)
+		job = my_cron.new(command='cd ' + str(dir_path) + " && " + "python3 auto.py -u " + str(username) + " -p " + str(password) + ' -t ' + str(protocol) + ' -c ' + str(camerapath) +  ' -l ' + str(length) + ' -e ' + str(eventlast) + ' -n ' + str(correction) + ' -f ' + str(fps), comment=launched)
+		job.hour.on(hour)
+		job.minute.on(minutes)
+
+		my_cron.write()
+		print('\nThank you... Recording will start at: ' + str(cronwhen))
+		sys.exit()
+
+
+
+	print("Starting process...")
+
+	with open(str(logpath) + str(created), 'a') as logfile:
+		log = ('\n' + str(finalname) + '\nFps:' + str(fps) + '\nEventlast:' + str(eventlast) + '\nFinalVideo:' + str(length) + '\nNbframe:' + str(nbframe) + '\nInterval:' + str(askedinterval))
+		logfile.write(log)
+		logfile.close()
+
+	while (i < nbframe):
+
+		try:
+
+			# Get info for how long left
+			frameleft = nbframe - i
+			timeleft = frameleft * askedinterval / 60
+			timeleft = round(timeleft, 0)
+			# Grab the frame
+			framecapture(username, password, protocol, camerapath, interval, nbframe, created, savepath, i)
+			time.sleep(interval)
+			print ("\n" * 200)
+			print ("[+] Current time: " + time.strftime("%c"))
+			print (" [|] Process started: " + str(launched))
+			print (" [|] Event length: " + str(eventlast) + " h.")
+			print (" [|] Final Video length: " + str(length) + " sec.")
+			print ("\n[+] Video specs: ")
+			print (" [|] Fps: " + str(fps) +".")
+			print (" [|] Number of frames: " + str(nbframe) + ".") 
+			print (" [|] Interval between shots: " + str(askedinterval) + " sec.")
+			print ("")
+			print ("\n[+] " + str(i) + "/" + str(nbframe) +" Frames captured")
+			print (" [|] " + str(timeleft) + " minutes left")
+			i = i + 1
+		except Exception as e:
+			print (e)
+		except KeyboardInterrupt:
+			print ("\n[!] Keyboard Interrupt ! ")
+			videoexport(fps, finalname, created, outputpath, savepath)
+			sys.exit()
+
+	# Capture is over, putting all the images together to create video file
+	videoexport(fps, finalname, created, outputpath, savepath)
+	print("\n[!] Success !!!")
+
+
+
+else:
+	print ('\nAbording Capture...')
+
